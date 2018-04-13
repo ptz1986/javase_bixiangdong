54fca0103159b8b5107f7624b2a96f1f7215b030
ptz1986 <34560685+ptz1986@users.noreply.github.com>
2018-04-13 20:42:41 +0800

    day2 java variable

---
+++ day2/11/VarDemo.java
@@ -0,0 +1,8 @@
+class VarDemo {
+	public static void main(String[] args)
+	{
+		byte b = 3;
+		b = b + 4;
+		System.out.println(b);
+	}
+}
---
+++ day2/13/VarDemo2.java
@@ -0,0 +1,14 @@
+class VarDemo2 {
+	public static void main(String[] args)
+	{
+		short b = 4;
+		byte b1 = 3;
+		byte b2 = 7;
+		b = b1 + b2;
+
+		int x = 2;
+		int x1 = 3;
+		int x2 = 4;
+		x = x1 + x2;
+	}
+}

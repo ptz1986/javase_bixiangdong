a970f5ec1b87275f455ed2dc1432fdb2b5c20906
ptz1986 <34560685+ptz1986@users.noreply.github.com>
2018-04-14 10:06:30 +0800

    day4 function memory array

---
+++ day4/24/ArrayDemo2.java
@@ -0,0 +1,11 @@
+class ArrayDemo2 {
+	public static void main(String[] args)
+	{
+
+		int[] arr = new int[3];
+		//arr = null;
+		//System.out.println(arr[2]);
+		System.out.println(arr);
+		
+	}
+}
